75f432c8e1a376fa4a457a658ad4fc56f666decf
Biseray <allan.biseray@outlook.fr>
2023-06-29 18:22:13 +0200

    add project vue

---
+++ vue-project/src/App.vue
@@ -0,0 +1,40 @@
+
+
+<template>
+  <HelloWorld :user="user"  />
+   <input type="text" v-model="user.name" @change="display">
+    <input type="number" v-model="user.age">
+
+
+
+    <div v-if="user.age < 18"> 
+  trop jeune</div>
+<div v-else="user.age> 18 ">
+trop vieux
+</div>
+</template>
+
+
+
+<script setup>
+import HelloWorld from '@/components/HelloWord.vue'
+import { reactive } from 'vue';
+
+
+const user = reactive({
+  name: 'allan',
+  age: '33'
+});
+
+
+const display = () => {
+  console.log(user.name);
+}
+
+
+
+display();
+</script>
+<style scoped>
+
+</style>
---
+++ vue-project/src/components/HelloWord.vue
@@ -0,0 +1,13 @@
+<template>
+    <h1>Hello World ! </h1>
+    <p>{{ props.user.name }} {{ props.user.age }}</p>
+</template>
+
+
+
+
+<script setup>
+const props = defineProps(['user']);
+
+</script>
+<style scoped></style>
